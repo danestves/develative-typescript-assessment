0414df06701587e67fb06046c37bfe71b7078612
danestves <danestves@users.noreply.github.com>
2024-11-09 22:24:31 -0400

    feat: better server with epic stack config

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,23 @@
-import { type LinksFunction } from '@remix-run/node'
+import {
+	type LoaderFunctionArgs,
+	type HeadersFunction,
+	type LinksFunction,
+} from '@remix-run/node'
 import {
 	Links,
 	Meta,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
+	useLoaderData,
 } from '@remix-run/react'
+import { HoneypotProvider } from 'remix-utils/honeypot/react'
+import { ClientHintCheck, getHints } from './utils/client-hints.tsx'
+import { getEnv } from './utils/env.server.ts'
+import { honeypot } from './utils/honeypot.server.ts'
+import { combineHeaders, getDomainUrl } from './utils/misc.tsx'
+import { useNonce } from './utils/nonce-provider.ts'
+import { makeTimings } from './utils/timing.server.ts'
 
 export const links: LinksFunction = () => [
 	{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -20,24 +32,90 @@ export const links: LinksFunction = () => [
 	},
 ]
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export async function loader({ request }: LoaderFunctionArgs) {
+	const timings = makeTimings('root loader')
+
+	const honeyProps = honeypot.getInputProps()
+
+	return Response.json(
+		{
+			requestInfo: {
+				hints: getHints(request),
+				origin: getDomainUrl(request),
+				path: new URL(request.url).pathname,
+			},
+			ENV: getEnv(),
+			honeyProps,
+		},
+		{
+			headers: combineHeaders({ 'Server-Timing': timings.toString() }),
+		},
+	)
+}
+
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+	const headers = {
+		'Server-Timing': loaderHeaders.get('Server-Timing') ?? '',
+	}
+	return headers
+}
+
+function Document({
+	children,
+	nonce,
+	env = {},
+}: {
+	children: React.ReactNode
+	nonce: string
+	env?: Record<string, string>
+}) {
+	const allowIndexing = ENV.ALLOW_INDEXING !== 'false'
 	return (
-		<html lang="en">
+		<html lang="en" className="h-full overflow-x-hidden">
 			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<ClientHintCheck nonce={nonce} />
 				<Meta />
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				{allowIndexing ? null : (
+					<meta name="robots" content="noindex, nofollow" />
+				)}
 				<Links />
 			</head>
 			<body>
 				{children}
-				<ScrollRestoration />
-				<Scripts />
+				<script
+					nonce={nonce}
+					dangerouslySetInnerHTML={{
+						__html: `window.ENV = ${JSON.stringify(env)}`,
+					}}
+				/>
+				<ScrollRestoration nonce={nonce} />
+				<Scripts nonce={nonce} />
 			</body>
 		</html>
 	)
 }
 
-export default function App() {
-	return <Outlet />
+function App() {
+	// if there was an error running the loader, data could be missing
+	const data = useLoaderData<typeof loader | null>()
+	const nonce = useNonce()
+
+	return (
+		<Document nonce={nonce} env={data?.ENV}>
+			<Outlet />
+		</Document>
+	)
 }
+
+function AppWithProviders() {
+	const data = useLoaderData<typeof loader>()
+	return (
+		<HoneypotProvider {...data.honeyProps}>
+			<App />
+		</HoneypotProvider>
+	)
+}
+
+export default AppWithProviders
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,50 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { flatRoutes } from 'remix-flat-routes'
+import { defineConfig } from 'vite'
+import { envOnlyMacros } from 'vite-env-only'
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+const MODE = process.env.NODE_ENV
 
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
-});
+	build: {
+		cssMinify: MODE === 'production',
+
+		rollupOptions: {
+			external: [/node:.*/, 'fsevents'],
+		},
+
+		sourcemap: true,
+	},
+	plugins: [
+		envOnlyMacros(),
+		remix({
+			ignoredRouteFiles: ['**/*'],
+			serverModuleFormat: 'esm',
+			future: {
+				unstable_optimizeDeps: true,
+				v3_fetcherPersist: true,
+				v3_lazyRouteDiscovery: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
+				v3_singleFetch: true,
+			},
+			routes: async (defineRoutes) => {
+				return flatRoutes('routes', defineRoutes, {
+					ignoredRouteFiles: [
+						'.*',
+						'**/*.css',
+						'**/*.test.{js,jsx,ts,tsx}',
+						'**/__*.*',
+						// This is for server-side utilities you want to colocate
+						// next to your routes without making an additional
+						// directory. If you need a route that includes "server" or
+						// "client" in the filename, use the escape brackets like:
+						// my-route.[server].tsx
+						'**/*.server.*',
+						'**/*.client.*',
+					],
+				})
+			},
+		}),
+	],
+})
